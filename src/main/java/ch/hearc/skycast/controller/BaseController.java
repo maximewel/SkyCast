edafbe3f86e58e1a27928392d715ff31a6423d52
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-02-16 17:19:34 +0100

    initial commit

---
+++ src/main/java/ch/hearc/skycast/controller/BaseController.java
@@ -0,0 +1,19 @@
+package ch.hearc.skycast.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/skycast")
+
+public class BaseController {
+	//dummy get
+	@GetMapping(value={"", "/", "/index"})
+	public String index(Model model, HttpSession session){
+		return "index";
+	}
+}
